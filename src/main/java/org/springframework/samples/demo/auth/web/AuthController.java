feeaf1271b27df0e2cb426476907c6c643d0cbaf
wdongyu <997473700@qq.com>
2018-04-26 16:24:00 +0800

    enable a new version of service running in background

--- src/main/java/org/springframework/samples/demo/auth/web/AuthController.java
+++ src/main/java/org/springframework/samples/demo/auth/web/AuthController.java
@@ -12,15 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 import java.util.HashMap;
-import java.io.BufferedReader;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.List;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import java.util.Random;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Properties;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  * @author wdongyu
@@ -66,6 +71,58 @@ public class AuthController {
             return "Fail";
         }
     }
+
+    @RequestMapping(value = "/servicePath" ,method = RequestMethod.GET)
+    @CrossOrigin(origins = "*")
+    public JSONObject servicePath() {
+        Properties properties = System.getProperties();
+        String path = properties.getProperty("user.dir");
+        String res = "{\"path\" : \"" + path + "\"}";
+
+        //execCommand("/usr/bin/open -a /Applications/Utilities/Terminal.app");
+        execCommand("git pull");
+        execCommand("mvn package");
+        execCommand("nohup java -jar ./target/spring-demo-auth-service-1.5.6.jar &");
+        //Process pro = null;
+        try {
+            //pro = rt.exec("mvn spring-boot:run", null, new File(path));
+            //ProcessBuilder pb = new ProcessBuilder(cmdString);
+            //pb.start();
+            return (JSONObject)(new JSONParser().parse(res));
+        } catch (Exception e) {
+            //TODO: handle exception
+            logger.info(e);
+        }
+        
+        return null;
+    }
+
+
+    private void execCommand(String command) {
+        try {
+            Process pro = Runtime.getRuntime().exec(command);
+            if (pro != null) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(pro.getInputStream()));
+                PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(pro.getOutputStream())), true);
+                try {
+                   String line;
+                   while ((line = in.readLine()) != null) {
+                      logger.info(line);
+                   }
+                   pro.waitFor();
+                   in.close();
+                   out.close();
+                   pro.destroy();
+                }
+                catch (Exception e) {
+                   logger.info("aaa");
+                }
+             }
+        } catch (Exception e) {
+            logger.info("fail to exec " + command);
+        }
+    }
+
 
     public String serviceInfo() {
         //List<ServiceInstance> list = this.client.getInstances(serviceName);

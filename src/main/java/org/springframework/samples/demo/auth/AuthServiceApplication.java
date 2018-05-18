25f20b566d09c46d8d4b7842c6f4be9a0c27ab6e
wdongyu <997473700@qq.com>
2018-05-18 14:43:05 +0800

    add sleuth dependency

--- src/main/java/org/springframework/samples/demo/auth/AuthServiceApplication.java
+++ src/main/java/org/springframework/samples/demo/auth/AuthServiceApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author wdongyu
@@ -11,6 +13,11 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class AuthServiceApplication {
+
+    @Bean 
+    public AlwaysSampler defaultSampler() {
+        return new AlwaysSampler();
+    }
 
     public static void main(String[] args) {
 	new SpringApplicationBuilder(AuthServiceApplication.class).web(true).run(args);
